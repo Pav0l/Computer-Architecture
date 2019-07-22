0ee0a2df41bd1e28153c3cc50af141c155923f7c
Pav0l <pavol.karas@gmail.com>
2019-07-22 15:35:39 +0200

    Args check from CLI

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -5,7 +5,13 @@
 import sys
 from cpu import *
 
-cpu = CPU()
 
-cpu.load()
-cpu.run()
+if len(sys.argv) < 2:
+    print("Missing CLI argument. Try something like: 'python ls8/ls8.py examples/print8.ls8'")
+elif sys.argv[0] == "ls8.py":
+    cpu = CPU()
+
+    cpu.load(sys.argv[1])
+    cpu.run()
+else:
+    print("Unknown file. Try running 'python ls8/ls8.py arg', where arg is path to file with instructions")
